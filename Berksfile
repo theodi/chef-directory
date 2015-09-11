adbc94c32f133c3aac557233a9066de382494e77
pikesley <github@orgraphone.org>
2015-09-11 11:15:05 +0100

    Refactoring is fun

--- Berksfile
+++ Berksfile
@@ -2,6 +2,8 @@ source "https://supermarket.chef.io"
 
 metadata
 
+cookbook 'skellington', github: 'theodi/chef-skellington'
+
 cookbook 'ruby-ng', github: '3ofcoins/chef-cookbook-ruby-ng'
 cookbook 'odi-users', github: 'theodi/chef-odi-users'
 cookbook 'envbuilder', github: 'theodi/chef-envbuilder'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,21 +2,7 @@ user = node['user']
 group = node['user']
 fqdn = node['fully_qualified_domain_name']
 
-include_recipe 'apt'
-include_recipe 'git'
-include_recipe 'nginx'
-include_recipe 'chef-client::config'
-include_recipe 'chef-client::cron'
-include_recipe 'odi-pk'
-include_recipe 'odi-users'
-include_recipe 'envbuilder'
-
-include_recipe 'ruby-ng'
-include_recipe 'ruby-ng::dev' unless node['ruby-ng']['dev_package']
-
-include_recipe 'chef_directory::dependencies'
-
-include_recipe 'odi-monitoring'
+include_recipe 'skellington'
 
 deploy_revision "/home/#{user}/#{fqdn}" do
   repo "git://github.com/#{node['repo']}"
