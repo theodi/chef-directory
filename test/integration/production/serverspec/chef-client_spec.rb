11879426193ee2439dddf352c4812cc030ac6596
pikesley <github@orgraphone.org>
2015-10-02 13:32:29 +0100

    We don;t do this anymore

--- test/integration/production/serverspec/chef-client_spec.rb
+++ test/integration/production/serverspec/chef-client_spec.rb
@@ -3,7 +3,7 @@ set :backend, :exec
 
 describe file '/etc/cron.d/chef-client' do
   it { should be_file }
-  its(:content) { should match /\/usr\/bin\/chef-client > \/var\/log\/chef\/cron.log/ }
+  its(:content) { should match /\/usr\/bin\/chef-client/ }
 end
 
 describe file '/etc/chef/client.rb' do
