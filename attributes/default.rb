9d06cecc139bd96e73f5fce25cc85b676d59a515
pikesley <github@orgraphone.org>
2015-10-02 13:31:52 +0100

    This has all moved to Skellington

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,8 @@ default['fully_qualified_domain_name'] = 'directory.theodi.org'
 default['catch_and_redirect'] = ['members.theodi.org']
 default['start_port'] = 8000
 default['concurrency'] = 2
+default['concurrency_string'] = "thin=#{node['concurrency']},worker=1"
+
 default['repo'] = 'theodi/member-directory'
 
 default['mysql']['database'] = 'member_directory_production'
@@ -10,18 +12,6 @@ default['mysql']['user'] = 'member_directory'
 default['mysql']['pool'] = 50
 
 default['migrate'] = 'bundle exec rake db:migrate'
-default['deployment']['rack_env'] = 'production'
-default['deployment']['revision'] = 'CURRENT'
-
-default['chef_client']['init_style'] = 'none'
-default['chef_client']['cron']['use_cron_d'] = true
-default['chef_client']['cron']['hour'] = '*'
-default['chef_client']['cron']['minute'] = '*/10'
-default['chef_client']['cron']['log_file'] = '/var/log/chef/cron.log'
 
 default['ruby-ng']['ruby_version'] = '1.9.3'
 default['ruby-ng']['dev_package'] = '1.9.1'
-
-default['envbuilder']['base_dir'] = '/home/directory'
-default['envbuilder']['owner'] = 'directory'
-default['envbuilder']['group'] = 'directory'
