abeb49aa04698712b00863953e3a95c1764cb6f9
pikesley <github@orgraphone.org>
2015-10-02 14:18:52 +0100

    Gone to skellington

--- definitions/precompile_assets.rb
+++
@@ -1,9 +0,0 @@
-define :precompile_assets, :params => {} do
-  bash 'Precompiling assets' do
-    cwd params[:cwd]
-    user params[:user]
-    code <<-EOF
-      bundle exec rake assets:precompile
-    EOF
-  end
-end
