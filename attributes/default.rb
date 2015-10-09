7ae2e58c20a8fccbd8b75e698adf9e2bea5403d0
pikesley <github@orgraphone.org>
2015-10-09 14:23:02 +0100

    Neaten up

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,12 @@
 default['user'] = 'directory'
-default['fully_qualified_domain_name'] = 'directory.theodi.org'
-default['catch_and_redirect'] = ['members.theodi.org']
+default['fully_qualified_domain_name'] = "#{node['user']}.theodi.org"
+default['repo'] = "#{node['github_org']}/member-directory"
 default['start_port'] = 8000
 default['concurrency'] = 2
 default['concurrency_string'] = "thin=#{node['concurrency']},worker=1"
+default['catch_and_redirect'] = ['members.theodi.org']
 
-default['repo'] = 'theodi/member-directory'
+default['ruby-ng']['ruby_version'] = '1.9.3'
 
 default['mysql']['database'] = 'member_directory_production'
 default['mysql']['user'] = 'member_directory'
@@ -13,6 +14,3 @@ default['mysql']['pool'] = 50
 
 default['migrate'] = 'bundle exec rake db:migrate'
 default['precompile_assets'] = true
-
-default['ruby-ng']['ruby_version'] = '1.9.3'
-default['ruby-ng']['dev_package'] = '1.9.1'
