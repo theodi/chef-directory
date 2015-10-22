a64d65eefe08603057ac7e8887068394284faa17
pikesley <github@orgraphone.org>
2015-10-22 09:57:28 +0100

    This works
    But I need to fix Skellington, and also introduce Spatula here

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 name "chef_directory"
-version "0.1.6"
+version "0.1.7"
 
 depends 'skellington'
--- test/sql/crebas.sql
+++
@@ -1,3 +0,0 @@
-create database if not exists directory;
-grant all on directory.* to 'directory' identified by 'directory';
-flush privileges;
