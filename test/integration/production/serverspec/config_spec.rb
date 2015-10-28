397c0dd8fe73d43f9623bb250ad13b30e99ee2de
pikesley <github@orgraphone.org>
2015-10-28 16:00:38 +0000

    This doesn't work because the hostname changes

--- test/integration/production/serverspec/config_spec.rb
+++ test/integration/production/serverspec/config_spec.rb
@@ -9,5 +9,4 @@ describe file '/home/directory/directory.theodi.org/shared/config/database.yml' 
   its(:content) { should match /database: member_directory_production/ }
   its(:content) { should match /username: member_directory/ }
   its(:content) { should match /pool: 50/ }
-  its(:md5sum) { should eq '49a2810809f2d09694aeb9fe27e9dd7f' }
 end
